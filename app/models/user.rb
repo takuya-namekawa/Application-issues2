802f8a6fb959c883f6f2ab464a0b30b135426787
EC2 Default User <ec2-user@ip-172-31-21-250.us-west-1.compute.internal>
2022-07-23 14:24:43 +0000

    [Relationship]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,35 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many  :books
+  has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :relationships, class_name: "Relationship",foreign_key: :follower_id ,dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id  ,dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
+
+
+
+
+
+  def follow(user_id)
+    relationships.create!(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+   followings.include?(user)
+  end
+
+
+
+
 
   has_one_attached :profile_image
 
@@ -18,4 +44,9 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+
+
+
+
 end
